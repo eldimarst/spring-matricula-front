6e509d2b8ab9be1e9c3f4df0c607d51ab2211335
eldimarst <eldimarst@gmail.com>
2024-09-14 23:10:55 -0300

    Opção de alterar cadastro de aluno

--- js/exemplo-escola.js
+++ js/exemplo-escola.js
@@ -5,9 +5,11 @@ function exibirAlerta(mensagem) {
         'color': '#721c24',
         'border': '1px solid #f8d7da'
     }).fadeIn();
+
+    // Oculta a mensagem após 4 segundos (4000 milissegundos)
     setTimeout(function() {
         $('#alerta').fadeOut();
-    }, 4000); // Desaparece após 4 segundos (4000 ms)
+    }, 4000);
 }
 
 // Exibe o alerta sucesso
@@ -17,9 +19,22 @@ function exibirSucesso(mensagem) {
         'color': '#155724',
         'border': '1px solid #c3e6cb'
     }).fadeIn();
+
+    // Oculta a mensagem após 3 segundos (3000 milissegundos)
     setTimeout(function() {
         $('#alerta').fadeOut();
     }, 3000); // Desaparece após 3 segundos (3000 ms)
+}
+
+function exibirAlertaModal(error){
+    $('#errorModalMessage').text(error);
+    $('#errorModalMessage').css('display', 'block');
+    
+    // Oculta a mensagem após 3 segundos (3000 milissegundos)
+    setTimeout(function() {
+        $('#errorModalMessage').text('');
+        $('#errorModalMessage').css('display', 'none');
+    }, 3000);
 }
 
 //faz a exclusão de registros
@@ -48,10 +63,40 @@ async function excluirRegistro(codigo) {
     }
 }
 
+// Funcao para abrir modal de alteração
+function openModalAlterar(codAluno) {
+    fetch('http://localhost:8080/api/alunos/obter?codAluno=' + codAluno, {
+        method: 'GET'
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error('Erro: ' + response.statusText);
+        }
+        return response.json();
+    }).then(data => {
+        $("#modalCodAluno").val(data.codAluno);
+        $("#modalNome").val(data.nome);
+        $("#modalCpf").val(data.cpf);
+        $("#modalDataNasc").val(data.dataNasc);
+        $("#modalCep").val(data.cep);
+        $("#modalLogradouro").val(data.logradouro);
+        $("#modalNumero").val(data.numero);
+        $("#modalBairro").val(data.bairro);
+        $("#modalCidade").val(data.cidade);
+        $("#modalUf").val(data.uf);
+
+        // Exiba a janela modal
+        $("#insertUpdateModal").modal("show");
+
+    }).catch(error => {
+        exibirAlerta(error.message);
+    });
+
+    
+}
 
 $(document).ready(function() {
 
-    //Ao carregar a modal de confirmação da exclusão
+    /////////////////////////////////////Ao carregar a modal de confirmação da exclusão manda alguns dados para tela
     $('#confirmDelModal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget); // Botão que acionou a modal
         var itemCodi = button.data('codigo'); // Extrai o valor do atributo data-codigo
@@ -61,13 +106,70 @@ $(document).ready(function() {
         modal.find('#confirmDelButton').data('codigoDel', itemCodi); // Define o valor de itemCodi no botão de confirmação
     });
 
-    //Ao clicar no botão de confirmação da exclusão dentro da Modal
+    /////////////////////////////////////Ao clicar no botão de confirmação da exclusão dentro da Modal
     $('#confirmDelButton').on('click', function () {
         var codigo = $(this).data('codigoDel'); // Obtém o valor de itemCodi
         excluirRegistro(codigo); // Chama a função com o valor de itemCodi
     });
 
-    //Ao clicar no botão de pesquisar alunos
+    /////////////////////////////////////Ao fazer submissao do formulario de alteração
+    $('#formCadastro').submit(function(event) {
+        event.preventDefault(); // Impede o envio padrão do formulário
+
+        var submitButton = event.target.querySelector('button[type="submit"]');
+        var spinner = submitButton.querySelector('.spinner-border');
+
+        // Mostra o spinner e desabilita o botão
+        spinner.style.display = 'inline-block';
+        submitButton.disabled = true;
+
+        // Captura os dados do formulário
+        const formData = {
+            codAluno: document.getElementById('modalCodAluno').value,
+            nome: document.getElementById('modalNome').value,
+            cpf: document.getElementById('modalCpf').value,
+            dataNasc: document.getElementById('modalDataNasc').value,
+            cep: document.getElementById('modalCep').value,
+            logradouro: document.getElementById('modalLogradouro').value,
+            numero: document.getElementById('modalNumero').value,
+            bairro: document.getElementById('modalBairro').value,
+            cidade: document.getElementById('modalCidade').value,
+            uf: document.getElementById('modalUf').value
+        };
+
+        // Envia os dados para o backend
+        fetch('http://localhost:8080/api/alunos/salvar', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+        .then(response => {
+            if (!response.ok) {
+                return response.text().then(text => { // Use response.text() para obter a mensagem de erro como texto
+                    throw new Error(text); 
+                });
+            }
+            
+            return response.json(); // Continua com a resposta JSON se a requisição foi bem-sucedida
+        })
+        .then(data => {
+            $('#insertUpdateModal').modal('hide');  // Fecha modal
+            $('#resultsTable').empty();             //Limpa tabela de pesquisa
+            exibirSucesso(`Operação realizada com sucesso`); //Exibe mensagem de sucesso
+        })
+        .catch((error) => {
+            exibirAlertaModal(error);
+        })
+        .finally(() => {
+            spinner.style.display = 'none';
+            submitButton.disabled = false;
+        });
+    });
+
+
+    /////////////////////////////////////Ao clicar no botão de pesquisar alunos
     $('#searchBtn').click(function() {
         const nome = $('#nome').val();
         const cpf = $('#cpf').val();
@@ -92,12 +194,14 @@ $(document).ready(function() {
             if (data) {
                 $('#resultsTable').empty();
                 
-                data.forEach(aluno => {
+                data.forEach(function(aluno, index) {
                     let dataNasc = new Date(aluno.dataNasc).toLocaleDateString('pt-BR', {
                         day: '2-digit',
                         month: '2-digit',
                         year: 'numeric'
                     });
+
+                    const uniqueId = `updateBtn${index + 1}`;
 
                     $('#resultsTable').append(`
                         <tr class="highlight">
@@ -106,11 +210,15 @@ $(document).ready(function() {
                             <td>${aluno.nome}</td>
                             <td>${dataNasc}</td>
                             <td>
-                                <button class="btn btn-warning btn-sm">Alterar</button>
+                                <button type="button" class="btn btn-warning btn-sm" id="${uniqueId}">Alterar</button>
                                 <button class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirmDelModal" data-nome="${aluno.nome}" data-codigo="${aluno.codAluno}">Excluir</button>
                             </td>
                         </tr>
                     `);
+
+                    $(`#${uniqueId}`).click(function() {
+                        openModalAlterar(aluno.codAluno);
+                    });
                 });
                 exibirSucesso(`${data.length} registros encontrados`);
             }
@@ -121,12 +229,48 @@ $(document).ready(function() {
         });
     });
 
+    /////////////////////////////////////Ao clicar no botão de incluir novo aluno
     $('#addBtn').click(function() {
         // Lógica para incluir um novo aluno
     });
 
+    /////////////////////////////////////Ao clicar no botão limpar pesquisa
     $('#clearBtn').click(function() {
         $('#searchForm')[0].reset();
         $('#resultsTable').empty();
     });
+
+    /////////////////////////////////////Busca CEP quando for preenchido no formulário da modal
+    $("#modalCep").blur(function() {
+        var cep = $(this).val().replace(/\D/g, '');
+        if (cep != "") {
+            var validacep = /^[0-9]{8}$/;
+            if(validacep.test(cep)) {
+                $("#modalLogradouro").val("...");
+                $("#modalBairro").val("...");
+                $("#modalCidade").val("...");
+                $("#modalUf").val("...");
+
+                fetch(`https://viacep.com.br/ws/${cep}/json/`)
+                .then(response => response.json())
+                .then(dados => {
+                    if (!dados.erro) {
+                        $("#modalLogradouro").val(dados.logradouro);
+                        $("#modalBairro").val(dados.bairro);
+                        $("#modalCidade").val(dados.localidade);
+                        $("#modalUf").val(dados.uf);
+                    } else {
+                        exibirAlertaModal("CEP não encontrado.");
+                    }
+                })
+                .catch(error => {
+                    exibirAlertaModal("Erro ao buscar o CEP:", erro);
+                });
+
+            } else {
+                exibirAlertaModal("Formato de CEP inválido.");
+            }
+        }
+    });
+
 });
